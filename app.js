32da03593ee197e624ab73996d3753e9e0f7bff4
ascyth <45745251+ascyth@users.noreply.github.com>
2022-09-11 17:42:23 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -26,14 +26,21 @@ const updatePage = async () => {
 
   // Make API request and get an array of fruit objects
   const fruitsArray = await apiRequest();
-  // console.log(fruitsArray);
+  console.log(fruitsArray);
 
   // TODO: Use either `map` and/or `filter` to extract some data from the array of fruit objects
   // For example, find "name of all fruits whose sugar > 15",
+  const highSugar = fruitsArray.filter(fruit => fruit.nutritions.sugar > 15).map(fruit => " " + fruit.name)
+  console.log(highSugar)
 
   // TODO: Create a new HTML element to display your data
+  var sugarElement = document.createElement('div');
+  sugarElement.innerHTML = '<h2>The Very Sugary Fruits</h2>';
+  sugarElement.innerHTML += highSugar;
 
   // TODO: Append your new element to the page
+  const existingElement = document.getElementById('list')
+  existingElement.append(sugarElement)
 
 }
 
